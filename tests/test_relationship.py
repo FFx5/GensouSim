31c9be387ca0ad19e5f2d3b7b66a7ad8f914247a
Jesse Foster <jessc788@yahoo.com>
2026-09-06 23:46:03 -0500

    Test relationship modification method

--- tests/test_relationship.py
+++ tests/test_relationship.py
@@ -19,4 +19,23 @@ relationship.trust = 8
 assert relationship.affinity == -4
 assert relationship.trust == 8
 
+relationship.modify(
+    affinity=2,
+    trust=-1,
+    respect=3,
+    fear=-1
+)
+
+assert relationship.affinity == -2
+assert relationship.trust == 7
+assert relationship.respect == 10
+assert relationship.fear == 0
+
+relationship.modify(affinity=2)
+
+assert relationship.affinity == 0
+assert relationship.trust == 7
+assert relationship.respect == 10
+assert relationship.fear == 0
+
 print("Relationship test passed.")
