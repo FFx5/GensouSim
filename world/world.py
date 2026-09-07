c2b782330880b6305e09dae2b2967096af77859e
Jesse Foster <jessc788@yahoo.com>
2026-09-06 21:43:39 -0500

    Implement character movement between locations

--- world/world.py
+++ world/world.py
@@ -1,3 +1,5 @@
+import random
+
 from simulation.clock import SimulationClock
 
 
@@ -27,13 +29,48 @@ class World:
     def add_character(self, character):
         self.characters[character.name] = character
 
+    def move_character(self, character):
+        """Move a character to a random connected location."""
+
+        if character.location not in self.locations:
+            return False
+
+        connections = list(
+            self.locations[character.location]["connections"]
+        )
+
+        if not connections:
+            return False
+
+        previous_location = character.location
+        character.location = random.choice(connections)
+
+        print(
+            f"[{self.current_time.strftime('%Y-%m-%d %H:%M:%S JST')}] "
+            f"{character.name} traveled from "
+            f"{previous_location} to {character.location}."
+        )
+
+        return True
+
     def tick(self):
         """Update the simulation."""
 
         current_time = self.clock.tick()
 
         for character in self.characters.values():
-            activity_changed = character.update(current_time, self)
+            activity_completed = (
+                character.activity_end_time is not None
+                and current_time >= character.activity_end_time
+            )
+
+            if activity_completed and character.activity in {
+                "Wandering",
+                "Exploring"
+            }:
+                self.move_character(character)
+
+            activity_changed = character.update(current_time)
 
             if activity_changed:
                 print(
