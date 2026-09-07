d1646c643cbcb65bfbad98d2f330dd3bb11f99f9
Jesse Foster <jessc788@yahoo.com>
2026-09-06 23:45:57 -0500

    Add relationship modification method

--- characters/relationship.py
+++ characters/relationship.py
@@ -10,3 +10,16 @@ class Relationship:
         self.trust = trust
         self.respect = respect
         self.fear = fear
+
+    def modify(
+        self,
+        affinity=0,
+        trust=0,
+        respect=0,
+        fear=0
+    ):
+        """Adjust relationship dimensions by the supplied amounts."""
+        self.affinity += affinity
+        self.trust += trust
+        self.respect += respect
+        self.fear += fear
