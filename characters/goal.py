0b9b705404094d6779b904c0f6680c83be5c5c10
Jesse Foster <jessc788@yahoo.com>
2026-09-06 22:00:44 -0500

    Connect goals to preferred activities

--- characters/goal.py
+++ characters/goal.py
@@ -1,8 +1,9 @@
 class Goal:
-    def __init__(self, name, description, priority=1):
+    def __init__(self, name, description, priority=1, preferred_activities=None):
         self.name = name
         self.description = description
         self.priority = priority
+        self.preferred_activities = preferred_activities or []
         self.completed = False
 
     def complete(self):
