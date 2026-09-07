8aab114dfbc9652b1c9fc588843cc86a0f51886b
Jesse Foster <jessc788@yahoo.com>
2026-09-06 21:47:08 -0500

    Update movement test for travel time

--- tests/test_movement.py
+++ tests/test_movement.py
@@ -12,7 +12,7 @@ world = World(clock_mode="manual")
 
 world.add_location("Hakurei Shrine")
 world.add_location("Human Village")
-world.connect_locations("Hakurei Shrine", "Human Village")
+world.connect_locations("Hakurei Shrine", "Human Village", travel_time=30)
 
 reimu = Character(
     "Reimu Hakurei",
@@ -39,7 +39,24 @@ world.tick()
 print()
 print(f"After wandering: {reimu.location} | Activity: {reimu.activity}")
 
+assert reimu.location == "Hakurei Shrine"
+assert reimu.activity == "Traveling"
+assert reimu.travel_destination == "Human Village"
+
+world.clock.advance(timedelta(minutes=29))
+world.tick()
+
+assert reimu.location == "Hakurei Shrine"
+assert reimu.activity == "Traveling"
+
+world.clock.advance(timedelta(minutes=1))
+world.tick()
+
+print()
+print(f"After traveling: {reimu.location} | Activity: {reimu.activity}")
+
 assert reimu.location == "Human Village"
+assert reimu.activity != "Traveling"
 
 print()
 print("Movement test passed.")
