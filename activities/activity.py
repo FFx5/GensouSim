5cbc1b68bf1d31af085a9e4a9b87aeacb52afa52
Jesse Foster <jessc788@yahoo.com>
2026-09-06 23:02:43 -0500

    Add generic world effects to activities

--- activities/activity.py
+++ activities/activity.py
@@ -12,7 +12,8 @@ class Activity:
         hunger_change_per_hour,
         energy_preference,
         hunger_preference,
-        allowed_locations=None
+        allowed_locations=None,
+        world_effects=None
     ):
         self.name = name
         self.minimum_duration = minimum_duration
@@ -22,6 +23,7 @@ class Activity:
         self.energy_preference = energy_preference
         self.hunger_preference = hunger_preference
         self.allowed_locations = allowed_locations
+        self.world_effects = world_effects or {}
 
     def get_duration(self):
         """Return a random duration within the activity's duration range."""
@@ -100,7 +102,11 @@ ACTIVITIES = {
             "medium": 0.9,
             "high": 0.7
         },
-        ["Hakurei Shrine"]
+        ["Hakurei Shrine"],
+        {
+            "maintenance_needed": False,
+            "last_maintenance_time": "current_time"
+        }
     ),
     "Resting": Activity(
         "Resting",
