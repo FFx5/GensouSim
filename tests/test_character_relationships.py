80ae7946e5edda42cfb5b16b56d1b1b421ac5f93
Jesse Foster <jessc788@yahoo.com>
2026-09-06 23:54:44 -0500

    Test character relationship modification

--- tests/test_character_relationships.py
+++ tests/test_character_relationships.py
@@ -6,6 +6,7 @@ reimu = Character("Reimu Hakurei")
 marisa = Character("Marisa Kirisame")
 youmu = Character("Youmu Konpaku")
 
+
 default_relationship = reimu.get_relationship(marisa)
 assert isinstance(default_relationship, Relationship)
 assert default_relationship.affinity == 0
@@ -38,5 +39,34 @@ assert marisa.get_relationship(reimu).affinity == 0
 
 marisa_relationship.affinity = 8
 assert reimu.get_relationship(marisa).affinity == 8
+
+reimu.modify_relationship(
+    marisa,
+    affinity=-3,
+    trust=2,
+    fear=1
+)
+
+assert reimu.get_relationship(marisa).affinity == 5
+assert reimu.get_relationship(marisa).trust == 5
+assert reimu.get_relationship(marisa).respect == 7
+assert reimu.get_relationship(marisa).fear == 2
+
+reimu.modify_relationship(
+    youmu,
+    affinity=4
+)
+
+assert reimu.get_relationship(youmu).affinity == 2
+
+reimu.modify_relationship(
+    Character("Alice Margatroid"),
+    affinity=3,
+    trust=1
+)
+
+alice = Character("Alice Margatroid")
+assert reimu.get_relationship(alice).affinity == 0
+assert reimu.get_relationship(alice).trust == 0
 
 print("Character relationship test passed.")
