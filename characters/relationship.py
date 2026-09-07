0292f64f966074bcc416a3208a5a1538ab9c8f4d
Jesse Foster <jessc788@yahoo.com>
2026-09-06 23:32:43 -0500

    Add relationship dimensions

---
+++ characters/relationship.py
@@ -0,0 +1,12 @@
+class Relationship:
+    def __init__(
+        self,
+        affinity=0,
+        trust=0,
+        respect=0,
+        fear=0
+    ):
+        self.affinity = affinity
+        self.trust = trust
+        self.respect = respect
+        self.fear = fear
